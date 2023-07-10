979db18006060202818534535025e6ab5f4d2f83
Vinicius Vaz <agvazvinicius@gmail.com>
2023-07-10 14:31:50 -0300

    error and user components

--- github-finder/src/routes/Home.tsx
+++ github-finder/src/routes/Home.tsx
@@ -1,16 +1,27 @@
 import { UserProps } from "../types/user";
 
 import Search from "../components/Search";
+import User from "../components/User";
+import Error from "../components/Error";
 
 import { useState } from "react";
 
 const Home = () => {
    const [user, setUser] = useState<UserProps | null>(null);
+   const [error, setError] = useState(false)
 
    const loadUser = async (userName: string) => {
+      setError(false)
+      setUser(null)
+      
       const res = await fetch(`https://api.github.com/users/${userName}`);
 
       const data = (await res.json()) as UserProps;
+
+      if (res.status === 404) {
+         setError(true)
+         return
+      }
 
       const { avatar_url, login, location, followers, following } = data;
 
@@ -27,8 +38,9 @@ const Home = () => {
    return (
       <div>
          <Search loadUser={loadUser} />
-         {user && <p>{user.login}</p>}
-      </div>
+         {user && <User {...user} />}
+         {error && <Error />}
+      </div >
    );
 };
 
